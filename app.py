128286d5880c8909fbe0a9852079456c787737cf
Omkar Singh <144312610+omijbhabha@users.noreply.github.com>
2024-11-14 20:40:19 +0000

    omkar

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import math
 from keras.layers import Input, LSTM, Dense
 from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
-import plotly.express as px
-import plotly.graph_objs as go
-import plotly.io as pio
 import base64
 from io import BytesIO
 
@@ -24,41 +21,42 @@ def submit_option():
     data = request.json
     option = data.get('option')
 
+    # Download stock data and preprocess
     stock = yf.download(option, period="3y", actions=True)
     data = stock.filter(['Close'])
     dataset = data.values
-
     training_data_len = math.ceil(len(dataset) * 0.75)
+
+    # Scaling the data
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(dataset)
 
+    # Prepare training data
     train_data = scaled_data[:training_data_len, :]
     x_train = []
     y_train = []
-
-    for i in range(60, len(train_data)):
-        x_train.append(train_data[i - 60:i, 0])
+    for i in range(30, len(train_data)):  # Reduced look-back window
+        x_train.append(train_data[i - 30:i, 0])
         y_train.append(train_data[i, 0])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
+    # Simplified LSTM model
     model = Sequential()
-    model.add(Input(shape=(x_train.shape[1], 1)))  # Define the input shape here
-    model.add(LSTM(50, return_sequences=True))
-    model.add(LSTM(50, return_sequences=False))
-    model.add(Dense(25))
+    model.add(Input(shape=(x_train.shape[1], 1)))
+    model.add(LSTM(20, return_sequences=False))  # Reduced LSTM units and layers
     model.add(Dense(1))
 
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=8, batch_size=1)
+    model.fit(x_train, y_train, epochs=3, batch_size=1, verbose=0)  # Fewer epochs
 
-    test_data = scaled_data[training_data_len - 60:, :]
+    # Prepare test data and make predictions
+    test_data = scaled_data[training_data_len - 30:, :]
     x_test = []
     y_test = dataset[training_data_len:, :]
-
-    for i in range(60, len(test_data)):
-        x_test.append(test_data[i - 60:i, 0])
+    for i in range(30, len(test_data)):
+        x_test.append(test_data[i - 30:i, 0])
 
     x_test = np.array(x_test)
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
@@ -66,13 +64,12 @@ def submit_option():
     predictions = model.predict(x_test)
     predictions = scaler.inverse_transform(predictions)
 
+    # Calculate average deviation
     rmse = np.sqrt(((predictions - y_test) ** 2).mean())
-
-    closes = []
-    closes.extend(data['Close'])
-    average_close = sum(closes) / len(closes)
+    average_close = data['Close'].mean()
     average_deviation = rmse / average_close * 100
 
+    # Plot and encode plot as base64
     train = data.iloc[:training_data_len]
     valid = data.iloc[training_data_len:]
     valid['Predictions'] = predictions
@@ -85,13 +82,11 @@ def submit_option():
     plt.plot(valid[['Close', 'Predictions']])
     plt.legend(['Train', 'Value', 'Predictions'], loc='lower right')
 
-    # Save plot to a BytesIO object and encode as base64
     img = BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
     plot_data = base64.b64encode(img.getvalue()).decode('utf-8')
 
-    # Return the result and the plot image in base64 format
     return jsonify({
         'message': f"Model prediction complete for {option}",
         'averageDeviation': average_deviation,
